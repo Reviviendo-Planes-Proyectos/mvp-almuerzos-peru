31f61c22626212aa1a2337b2ce454cb5a177e1d7
Luis Lizarme <Luislizarme00@gmail.com>
2025-07-21 15:09:59 -0500

    Documentación: Estructura sugerida para mover el corazón al lado del nombre del restaurante

--- public/js/index.js
+++ public/js/index.js
@@ -316,18 +316,24 @@ document.addEventListener("DOMContentLoaded", () => {
                     const totalLikes = restaurant.totalLikes || 0;
 
                     restaurantCard.innerHTML = `
-                                        <img src="${imageUrl}" alt="${restaurant.name}" class="restaurant-card-image">
-                                        
-                                        <div class="restaurant-likes-overlay">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-                                            <span>${totalLikes}</span>
-                                        </div>
+                               <img src="${imageUrl}" alt="${restaurant.name}" class="restaurant-card-image">
 
-                                        <div class="card-content">
-                                            <h4>${restaurant.name}</h4>
-                                            <p>${restaurant.description}</p>
-                                            <a href="/menu.html?restaurantId=${restaurant.id}" class="card-button">Ver menú</a>
-                                        </div>
+                <div class="card-content">
+   <div class="restaurant-title">
+  <h4>${restaurant.name}</h4>
+  <span class="restaurant-likes-inline">
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="red" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
+      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+    </svg>
+    <span>${totalLikes}</span>
+  </span>
+</div>
+
+
+               <p>${restaurant.description}</p>
+               <a href="/menu.html?restaurantId=${restaurant.id}" class="card-button">Ver menú</a>
+                </div>
+
                                     `;
                     restaurantsList.appendChild(restaurantCard);
                     setTimeout(() => {
